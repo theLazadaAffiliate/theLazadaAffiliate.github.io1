025ad2e5737d596918ac59ade3061e0c2fee6d8f
Kyru Cabading <kyrusri@gmail.com>
2018-02-20 13:28:10 +0800

    Push

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+
+// Typed.js
 $(function(){
 	$(".typed").typed({
 		strings: ["content", "Youtube videos", "Makeup reviews", "Facebook Page", "content"],
@@ -33,4 +35,36 @@ $(function(){
 		// callback for reset
 		resetCallback: function() {}
 	});
+});
+
+// Calculator
+
+$(document).ready(function($) {
+
+$('.item-type').click(function() {
+	$('.item-type').removeClass('item-type-active');
+  $(this).addClass('item-type-active');
+  });
+
+function rangeCalc(i) {
+var totalEarnings = 0;
+var clickRange = [{begin:1, price:0}, {begin:4, price:0}, {begin:8, price:38}, {begin:12, price:57}, {begin:20, price:95}, {begin:28, price:133}, {begin:40, price:190}, {begin:52, price:247}, {begin:60, price:285}, {begin:72, price:342}, {begin:92, price:437}, {begin:120, price:570}, {begin:140, price:665}, {begin:160, price:760}, {begin:172, price:817}, {begin:180, price:855}, {begin:192, price:912}, {begin:200, price:950},];
+
+clickRange.forEach(function(num, item) {
+	if (clickRange[item].begin <= i) {
+    totalEarnings = clickRange[item].price * 50;
+    $('.clicks').text(i*25);
+    $('.earnings').text("Php "+totalEarnings.toLocaleString()+" per month");
+    }
+  //console.log(tariff[item].begin);
+});
+}
+
+$('.calc-range').on('input', function() {
+	$('.links').text(this.value);
+  rangeCalc(this.value);
+});
+
+//rangeCalc();
+
 });
